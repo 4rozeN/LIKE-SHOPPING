d6fed139ac7a731d614f611da99042f89f0a37b5
4rozeN <1686617971@qq.com>
2024-09-04 23:53:05 +0800

    -配置了main.js的基地址和请求响应拦截器。 -初始化了npm包管理。 -设置了pages.json文件分包方式和分包根目录。新建subpkg为分包目录。 -新增了首页获取并渲染显示轮播图的功能代码。 -新增了商品详情页goods_detail用于测试分包。

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,13 @@
 <template>
   <view>
     我是首页
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+      <swiper-item v-for="item in swiperList" :key="item.goods_id">
+        <navigator class="swiper-item" :url="'/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id">
+          <image :src="item.image_src"></image>
+        </navigator>
+      </swiper-item>
+    </swiper>
   </view>
 </template>
 
@@ -8,12 +15,36 @@
   export default {
     data() {
       return {
-        
+        // 存放轮播图
+        swiperList: []
       };
+    },
+    async onLoad () {
+      // 开始加载时，得到轮播图数据
+      this.getSwiperList()
+    },
+    methods: {
+      async getSwiperList () {
+        const res = await uni.$http.get('/api/public/v1/home/swiperdata')
+        console.log('res:', res)
+        if (res.statusCode !== 200) {
+          return uni.$showMsg(res.meta.msg, 1500)
+        }
+        // 成功后进行数据挂载
+        this.swiperList = res.data.message
+        console.log('swiperList:', this.swiperList)
+      }
     }
   }
 </script>
 
 <style lang="scss">
-
+swiper {
+  height: 330rpx;
+  .swiper-item,
+  image {
+    height: 100%;
+    width: 100%;
+  }
+}
 </style>
