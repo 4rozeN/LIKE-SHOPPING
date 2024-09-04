c9a45e7b2160628ef63b3d132640c8fea5f5b34d
4rozeN <1686617971@qq.com>
2024-09-04 18:24:56 +0800

    -完成了基础的页面配置。配置了底部的tabBar

---
+++ pages/cart/cart.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    我是购物车页面
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
