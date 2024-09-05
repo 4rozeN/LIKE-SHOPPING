76e5630225af6314b3e8988c1ba1cf0d1cfe2e13
4rozeN <1686617971@qq.com>
2024-09-06 04:08:30 +0800

    -新增了商品自定义组件。 -新增了商品搜索列表页面。基本实现了搜索数据渲染。

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,10 @@
 <template>
   <view>
-    我是商品列表页
+    <view class="goods-list">
+      <block v-for="(goods, i) in goodsList" :key="goods.goods_name">
+        <my-goods :goods="goods"></my-goods>
+      </block>
+    </view>
   </view>
 </template>
 
@@ -8,12 +12,44 @@
   export default {
     data() {
       return {
-        
+        defaultPic: 'https://fakeimg.pl/320x240/?text=default-Image',
+        // 查询参数对象
+        queryObj: {
+          // 查询关键词
+          query: '',
+          // 商品分类ID
+          cid: '',
+          // 页数索引，页码值
+          pagenum: 1,
+          // 每页显示数据量
+          pagesize: 20,
+        },
+        // 商品列表
+        goodsList: [],
+        // 总数量，实现分页用到
+        total: 0
       };
+    },
+    onLoad (options) {
+      // 得到查询参数
+      this.queryObj.query = options.query || ''
+      this.queryObj.cid = options.cid || ''
+      // 获取商品列表数据
+      this.getGoodsList()
+    },
+    methods: {
+      // 获取商品列表数据
+      async getGoodsList () {
+        const res = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+        if (res.data.meta.status !== 200) { return res.data.meta.msg}
+        this.goodsList = res.data.message.goods
+        this.total = res.data.message.total
+        console.log('this.goodsList:', this.goodsList)
+        console.log('this.total:', this.total)
+      }
     }
   }
 </script>
 
 <style lang="scss">
-
 </style>
---
+++ uni_modules/my-goods/components/my-goods/my-goods.vue
@@ -0,0 +1,71 @@
+<template>
+  <view class="goods-item">
+    <!-- 商品左侧图片 -->
+    <view class="goods-item-left" style="margin-right: 5px;">
+      <image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
+    </view>
+    <!-- 右侧信息 -->
+    <view class="goods-item-right">
+      <!-- 标题 -->
+      <view class="goods-name">{{goods.goods_name}}</view>
+      <view class="goods-info-box">
+        <!-- 价格 -->
+        <view class="goods-price">￥{{goods.goods_price | tofixed}}</view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  props: {
+    goods: {
+      type: Object,
+      default: {}
+    }
+  },
+  data () {
+    return {
+      defaultPic: 'https://fakeimg.pl/320x240/?text=default-Image'
+    }
+  },
+  filters: {
+    tofixed (num) {
+      return Number(num).toFixed(2)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .goods-item {
+    display: flex;
+    padding: 10px 5px;
+    border-bottom: 1px solid #f0f0f0;
+    
+    .goods-item-left {
+      margin-right: 5px;
+      
+      .goods-pic {
+        width: 100px;
+        height: 100px;
+        display: block;
+      }
+    }
+    
+    .goods-item-right {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      
+      .goods-name {
+        font-size: 13px;
+      }
+      
+      .goods-price {
+        font-size: 16px;
+        color: #c00000;
+      }
+    }
+  }
+</style>
